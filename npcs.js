7ab082d9c7be96af383a97568849ade81d7401f0
Shamenox <Shamenox@gmail.com>
2017-01-27 13:11:07 +0100

    Klasse fertig, abbildung des Skins nicht möglich

--- npcs.js
+++ npcs.js
@@ -1,56 +1,49 @@
 ﻿var npc = {};
 function setupNpcs(){
-	function createNpc(texture){
-	neuerNpc = {};
-	neuerNpc.skin = texture;
-	neuerNpc.spawned = [];
-	neuerNpc.dir = right;
-	if (skin[texture] !== undefined) neuerNpc.framed = true;
-	if (skin[texture] === undefined) neuerNpc.framed = true;
-	neuerSkin.spawn = function(at){
-		if (this.spawned[scene.at] === undefined) this.x = at, this.spawned[scene.at] = true;
-		if (this.dir === "left") Game.ctx.drawImage(image.scp1730l, npc.scp173.x, groundlevel - 50*scale, 220*scale, 440*scale);
-
-		if (npc.scp173.dir === "right"){
-			if (player1.dir === "right") npc.scp173.x +=40, audio.concrete.play();
-			Game.ctx.drawImage(image.scp1730r, npc.scp173.x, groundlevel - 50*scale, 220*scale, 440*scale);
+	function createNpc(declaration, texture){
+		neuerNpc = {};
+		neuerNpc.skin = texture;
+		neuerNpc.spawned = [];
+		neuerNpc.dir = "right";
+		if (skin[texture] !== undefined) neuerNpc.framed = true;
+		if (skin[texture] === undefined) neuerNpc.framed = false;
+		neuerNpc.spawn = function(at){
+			if (this.spawned[scene.at] === undefined) this.x = at, this.spawned[scene.at] = true;
+			if (!this.framed){
+				console.log(this.skin, image[this.skin+"0l"]);
+				if (this.dir === "left") Game.ctx.drawImage(image[this.skin+"0l"], this.x, groundlevel - (image[this.skin+"0l"].naturalHeight * scale -440), image[this.skin+"l"].naturalWidth * scale, image[this.skin+"0l"].naturalHeight * scale);
+				if (this.dir === "right") Game.ctx.drawImage(image[this.skin+"0r"], this.x, groundlevel - (image[this.skin+"0r"].naturalHeight * scale -440), image[this.skin+"r"].naturalWidth * scale, image[this.skin+"0r"].naturalHeight * scale);
+			}
+			if (this.behaviour !== undefined) this.behaviour();
 		}
-		if (player1.x.between(npc.scp173.x +5 , npc.scp173.x -5)) die(), npc.scp173.setup === false;
+		npc[declaration] = neuerNpc;
 	}
 	
-	
-		
-	}
-	
-	npc.scp173 = { setup : false};
-	npc.scp173.spawn = function(at){
-		if (!npc.scp173.setup) npc.scp173.x = at, npc.scp173.setup = true;
+	createNpc("scp173", "scp173");
+	npc.scp173.behaviour = function(){
 		if (player1.x < npc.scp173.x) npc.scp173.dir = "left";
 		if (player1.x > npc.scp173.x) npc.scp173.dir = "right";
 		if (npc.scp173.dir === "left"){
 			if (player1.dir === "left") npc.scp173.x -=40, audio.concrete.play();
-			Game.ctx.drawImage(image.scp1730l, npc.scp173.x, groundlevel - 50*scale, 220*scale, 440*scale);
 		}
 		if (npc.scp173.dir === "right"){
 			if (player1.dir === "right") npc.scp173.x +=40, audio.concrete.play();
-			Game.ctx.drawImage(image.scp1730r, npc.scp173.x, groundlevel - 50*scale, 220*scale, 440*scale);
 		}
 		if (player1.x.between(npc.scp173.x +5 , npc.scp173.x -5)) die(), npc.scp173.setup === false;
 	}
-	npc.riyu = {};
+	
+	createNpc("riyu", "riyu");
 	npc.riyu.conversation1 = function(){
 		talk("Kiiro: Hi Riyu!", 1);
         talk("Riyu: Hey Kiiro, i was wondering if you would like to come over", 2);
         talk("Kiiro: Of course, but we will have to wait until the outside world is finnished...", 3);
         talk("Riyu: Yeah, they should really hurry up...", 4);
 	}
-	npc.riyu.spawn = function(at, conversation){
-		npc.riyu.x = at;
+	npc.riyu.behaviour = function(){
 		if (player1.x.between(npc.riyu.x + 100* scale, npc.riyu.x - 100* scale)) {
 			if (use === "false") Game.ctx.fillText("Talk(E)", npc.riyu.x + 110*scale, groundlevel);
 			if (use === "true") use = "talk";
-			if (use === "talk") npc.riyu[conversation]();
+			if (use === "talk") npc.riyu.conversation1();
 		}
-		Game.ctx.drawImage(image.riyu0r, npc.riyu.x, groundlevel,220*scale,440*scale);
 	}
 }
